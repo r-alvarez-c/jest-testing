c7e9af3227550359a6f18949c5e3ea9e92436b52
Raúl Ignacio Álvarez Cortés <raul.alvarezc@sansano.usm.cl>
2022-07-26 03:51:38 -0400

    added env.PORT for Azure

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
 app.set("view engine", "ejs");
+app.set('port',process.env.PORT || 3000)
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use("/", require("./router"));
 
-app.listen(3000, () => {
-  console.log("SERVER corriendo en http://localhost:3000");
+app.listen(app.get('port'), () => {
+  console.log("SERVER corriendo en puerto "+app.get('port'));
 });
